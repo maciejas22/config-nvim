bb37a86e5fc95290e7ae622f1333b94df56338c3
maciejas22 <motorosas2012@gmail.com>
2025-05-19 21:31:14 +0200

    feat: replace lualine with mini.statusline

--- lua/maciejas22/plugins/smart-splits.lua
+++ lua/maciejas22/plugins/smart-splits.lua
@@ -5,10 +5,10 @@ return {
     smart_splits.setup({})
 
     -- resizing splits
-    vim.keymap.set("n", "<C-S-h>", require("smart-splits").resize_left)
-    vim.keymap.set("n", "<C-S-j>", require("smart-splits").resize_down)
-    vim.keymap.set("n", "<C-S-k>", require("smart-splits").resize_up)
-    vim.keymap.set("n", "<C-S-l>", require("smart-splits").resize_right)
+    vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+    vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+    vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+    vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
     -- moving between splits
     vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
     vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
